f9de7a51e7eaf6b250dcde70032ff47a9ea9c552
magwindow <antosha.petrakov@yandex.ru>
2025-09-27 11:06:33 +0300

    Registration form & controller are ready

--- users/forms.py
+++ users/forms.py
@@ -1,13 +1,26 @@
 from django import forms
-from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
 
 from users.models import User
 
 
 class UserLoginForm(AuthenticationForm):
+    username = forms.CharField()
+    password = forms.CharField()
+
     class Meta:
         model = User
         fields = ('username', 'password')
 
+
+class UserRegistrationForm(UserCreationForm):
+    first_name = forms.CharField()
+    last_name = forms.CharField()
     username = forms.CharField()
-    password = forms.CharField()
+    email = forms.EmailField()
+    password1 = forms.CharField()
+    password2 = forms.CharField()
+
+    class Meta:
+        model = User
+        fields = ('first_name', 'last_name', 'username', 'email', 'password1', 'password2')
--- users/views.py
+++ users/views.py
@@ -1,9 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib import auth
 from django.http import HttpResponseRedirect
 from django.urls import reverse
 
-from users.forms import UserLoginForm
+from users.forms import UserLoginForm, UserRegistrationForm
 
 
 def login(request):
@@ -28,9 +28,21 @@ def login(request):
 
 
 def registration(request):
+    if request.method == 'POST':
+        form = UserRegistrationForm(data=request.POST)
+        if form.is_valid():
+            form.save()
+            user = form.instance
+            auth.login(request, user)
+            return HttpResponseRedirect(reverse('main:index'))
+    else:
+        form = UserRegistrationForm()
+
     context = {
         'title': 'HOME - Регистрация',
+        'form': form
     }
+
     return render(request, 'users/registration.html', context)
 
 
@@ -42,4 +54,5 @@ def profile(request):
 
 
 def logout(request):
-    ...
+    auth.logout(request)
+    return redirect(reverse('main:index'))
