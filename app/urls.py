366509dbea682fcae2811c937494af90df213bb3
magwindow <antosha.petrakov@yandex.ru>
2025-10-07 20:56:38 +0300

    Registered simple tag for categories

--- app/urls.py
+++ app/urls.py
@@ -1,5 +1,8 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.conf.urls.static import static
+
+from app import settings
 
 
 urlpatterns = [
@@ -7,3 +10,6 @@ urlpatterns = [
     path('', include('main.urls', namespace='main')),
     path('catalog/', include('goods.urls', namespace='catalog')),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- main/views.py
+++ main/views.py
@@ -1,13 +1,9 @@
 from django.shortcuts import render
 
-from goods.models import Categories
-
 def index(request):
-    categories = Categories.objects.all()
     context = {
         'title': 'Home - Главная',
-        'content': 'Магазин мебели HOME',
-        'categories': categories
+        'content': 'Магазин мебели HOME'
     }
     return render(request, 'main/index.html', context)
 
