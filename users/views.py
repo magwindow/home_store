7999992ba5875f990a8c95b6d44b6f197de98998
magwindow <antosha.petrakov@yandex.ru>
2025-10-08 19:22:20 +0300

    Login form and controller are ready

--- users/views.py
+++ users/views.py
@@ -1,9 +1,26 @@
+from django.urls import reverse
 from django.shortcuts import render
-from django.contrib.auth import login, logout
+from django.contrib import auth
+from django.http import HttpResponseRedirect
+
+from users.forms import UserLoginForm
 
 def login(request):
+    if request.method == 'POST':
+        form = UserLoginForm(data=request.POST)
+        if form.is_valid():
+            username = request.POST['username']
+            password = request.POST['password']
+            user = auth.authenticate(username=username, password=password)
+            if user:
+                auth.login(request, user)
+                return HttpResponseRedirect(reverse('main:index'))
+    else:
+        form = UserLoginForm()
+    
     context = {
-        'title': 'Home - Авторизация'     
+        'title': 'Home - Авторизация',
+        'form': form     
     }
     return render(request, 'users/login.html', context)
 
@@ -20,5 +37,4 @@ def profile(request):
     return render(request, 'users/profile.html', context)
 
 def logout(request):
-    logout(request)
-    return render(request, 'main/index.html')
+    pass
