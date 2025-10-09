dd5b2e6f8d395f60b2cf9456bd645d29462aedda
magwindow <antosha.petrakov@yandex.ru>
2025-10-09 17:47:16 +0300

    Create order is ready

--- users/admin.py
+++ users/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 
+from orders.admin import OrderTabulareAdmin
 from carts.admin import CartTabAdmin
 from users.models import User
 
@@ -9,4 +10,4 @@ class UserAdmin(admin.ModelAdmin):
     search_fields = ["username", "first_name", "last_name", "email",]
     
     
-    inlines = [CartTabAdmin,]
+    inlines = [CartTabAdmin, OrderTabulareAdmin]
