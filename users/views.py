1e98906b8ef7e5fe9bb1b53ba6d42cf02b6ba953
magwindow <antosha.petrakov@yandex.ru>
2025-10-09 14:10:43 +0300

    Sessions - carts for anonymous user

--- users/views.py
+++ users/views.py
@@ -4,6 +4,7 @@ from django.contrib import auth, messages
 from django.contrib.auth.decorators import login_required
 from django.http import HttpResponseRedirect
 
+from carts.models import Cart
 from users.forms import UserLoginForm, UserRegistrationForm, ProfileForm
 
 def login(request):
@@ -13,9 +14,15 @@ def login(request):
             username = request.POST['username']
             password = request.POST['password']
             user = auth.authenticate(username=username, password=password)
+            
+            session_key = request.session.session_key
+            
             if user:
                 auth.login(request, user)
                 messages.success(request, f"{username}, Вы вошли в аккаунт")
+                
+                if session_key:
+                    Cart.objects.filter(session_key=session_key).update(user=user)
                 
                 # Перенаправляет анонимного пользователя после авторизации
                 # на тот URL адрес, на который он хотел перейти
@@ -38,8 +45,15 @@ def registration(request):
         form = UserRegistrationForm(data=request.POST)
         if form.is_valid():
             form.save()
+            
+            session_key = request.session.session_key
+            
             user = form.instance
             auth.login(request, user)
+            
+            if session_key:
+                Cart.objects.filter(session_key=session_key).update(user=user)
+                
             messages.success(request, f"{user.username}, Вы успешно зарегистрированы и вошли в аккаунт")
             return HttpResponseRedirect(reverse('main:index'))
     else:
