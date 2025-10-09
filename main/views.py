aef319bda91aeeaecc4acbcfc1e8055ad10accff
magwindow <antosha.petrakov@yandex.ru>
2025-10-09 23:22:59 +0300

    Refactor controllers on class based views

--- main/views.py
+++ main/views.py
@@ -1,16 +1,21 @@
-from django.shortcuts import render
+from django.views.generic import TemplateView
 
-def index(request):
-    context = {
-        'title': 'Home - Главная',
-        'content': 'Магазин мебели HOME'
-    }
-    return render(request, 'main/index.html', context)
+class IndexView(TemplateView):
+    template_name = 'main/index.html'
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = 'Home - Главная'
+        context['content'] = "Магазин мебели HOME"
+        return context
+    
 
-def about(request):
-    context = {
-        'title': 'О нас',
-        'content': 'О нас',
-        'text_on_page': 'Текст о том почему этот магазин такой классный, и какой хороший товар.'
-    }
-    return render(request, 'main/about.html', context)
+class AboutView(TemplateView):
+    template_name = 'main/about.html'
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = 'Home - О нас'
+        context['content'] = "О нас"
+        context['text_on_page'] = "Текст о том почему этот магазин такой классный, и какой хороший товар."
+        return context
